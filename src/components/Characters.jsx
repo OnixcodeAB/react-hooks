65148704ba2f641dff40c7eab96020c235d2d2ff
Abel Mejia <onixocde.AB@gmail.com>
2023-06-20 16:01:59 -0400

    Configurando el uso de useReducer

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -6,12 +6,37 @@ import {
   CCardText,
   CCardTitle,
 } from "@coreui/react";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useReducer } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import "../styles/character.css";
+
+const initialState = {
+  favorites: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_FAVORITE":
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      };
+    default:
+      return state;
+  }
+};
 
 const Characters = ({ darkMode }) => {
   const [characters, setCharacters] = useState([]);
   const { darkmode } = useContext(ThemeContext);
+  const [favorites, dispatch] = useReducer(reducer, initialState);
+
+  const onClick = (favorite) => {
+    dispatch({
+      type: "ADD_TO_FAVORITE",
+      payload: favorite,
+    });
+  };
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
@@ -21,22 +46,47 @@ const Characters = ({ darkMode }) => {
 
   return (
     <div className="characters">
-      {characters.map((character) => (
-        <CCard
-          color={`${darkmode ? "dark" : "light"}`}
-          key={character.id}
-          style={{ width: "18rem" }}
-        >
-          <CCardImage orientation="top" src={character.image} />
-          <CCardBody>
-            <CCardTitle>{character.name}</CCardTitle>
-            <CCardText>{character.status}</CCardText>
-            <CCardText>{character.species}</CCardText>
-            <CCardText>{character.gender}</CCardText>
-            <CButton href="#">Go somewhere</CButton>
-          </CCardBody>
-        </CCard>
-      ))}
+      <div className="favorite">
+        {favorites.favorites.map((favorite) => (
+          <CCard
+            color={`${darkmode ? "dark" : "light"}`}
+            key={favorite.id}
+            style={{ width: "18rem" }}
+          >
+            <CCardImage orientation="top" src={favorite.image} />
+            <CCardBody>
+              <CCardTitle>{favorite.name}</CCardTitle>
+              <CCardText>{favorite.status}</CCardText>
+              <CCardText>{favorite.species}</CCardText>
+              <CCardText>{favorite.gender}</CCardText>
+              <CButton type="button" onClick={() => onClick(favorite)}>
+                Add Favorite
+              </CButton>
+            </CCardBody>
+          </CCard>
+        ))}
+      </div>
+
+      <div className="list">
+        {characters.map((character) => (
+          <CCard
+            color={`${darkmode ? "dark" : "light"}`}
+            key={character.id}
+            style={{ width: "18rem" }}
+          >
+            <CCardImage orientation="top" src={character.image} />
+            <CCardBody>
+              <CCardTitle>{character.name}</CCardTitle>
+              <CCardText>{character.status}</CCardText>
+              <CCardText>{character.species}</CCardText>
+              <CCardText>{character.gender}</CCardText>
+              <CButton type="button" onClick={() => onClick(character)}>
+                Add Favorite
+              </CButton>
+            </CCardBody>
+          </CCard>
+        ))}
+      </div>
     </div>
   );
 };
